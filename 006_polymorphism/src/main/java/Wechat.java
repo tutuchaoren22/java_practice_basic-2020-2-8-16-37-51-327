70b94964a307416afe57d09b48cffee186b8aba2
duyuqian <15738381799@163.com>
2020-02-10 20:32:54 +0800

    [duyuqian] feat: create the Wechat class extends AppOnWindows as subclass

---
+++ 006_polymorphism/src/main/java/Wechat.java
@@ -0,0 +1,10 @@
+public class Wechat extends AppOnWindows {
+    public Wechat() {
+    }
+
+    @Override
+    public void printF5() {
+        super.printF5();
+        System.out.println("弹出英文输出框");
+    }
+}
